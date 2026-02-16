54bbff249e73898e727dc387d9b2ed0ab0785f67
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-16 17:55:08 +0530

    added git hub and live demo link styling

--- src/Layoutcomponents/Project/Card.jsx
+++ src/Layoutcomponents/Project/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Card({ cardName, image, projectInfo }) {
+function Card({ cardName, image, projectInfo, githublink, liveDemoLink }) {
   return (
     <div className="w-80 bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300">
       {/* Image */}
@@ -17,6 +17,27 @@ function Card({ cardName, image, projectInfo }) {
         <h2 className="text-xl font-semibold text-gray-800 mb-2">{cardName}</h2>
 
         <p className="text-gray-600 text-sm">{projectInfo}</p>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex gap-3 mt-3">
+        <a
+          href={githublink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex-1 text-center px-4 py-2 text-sm font-medium border border-purple-900 text-purple-600 rounded-lg  "
+        >
+          GitHub
+        </a>
+
+        <a
+          href={liveDemoLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex-1 text-center px-4 py-2 text-sm font-medium bg-purple-900 text-white rounded-lg hover:bg-purple-500 transition-all duration-300"
+        >
+          Live Demo
+        </a>
       </div>
     </div>
   );
