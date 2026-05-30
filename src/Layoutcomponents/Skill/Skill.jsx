f72cd1cce61502d73e18392a3af08abc49cb0137
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-05-30 23:58:51 +0530

    fixed node js icon bug

--- src/Layoutcomponents/Skill/Skill.jsx
+++ src/Layoutcomponents/Skill/Skill.jsx
@@ -78,7 +78,7 @@ function Skill() {
               Backend Development
             </h2>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 justify-items-center">
-              <Icons iconname="node" />
+              <Icons iconname="nodedotjs" />
               <Icons iconname="express" />
             </div>
           </div>
